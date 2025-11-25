e05a3353e3e3a3d85970464cc105f2cc5e232cc8
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-11-26 04:44:18 +0700

    fix `fish` shell not complete commands

--- home/truong/shell.nix
+++ home/truong/shell.nix
@@ -7,6 +7,8 @@
         fish = {
             generateCompletions = true;
             interactiveShellInit = ''
+                set -p fish_complete_path ${pkgs.fish}/share/fish/completions
+
                 set FLINE_PATH $XDG_CONFIG_HOME/fish/fishline
                 source $FLINE_PATH/init.fish
             '';
