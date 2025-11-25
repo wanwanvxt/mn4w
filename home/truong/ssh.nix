78841af229cd100e9c67294a3134ed12099ba0fb
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-11-26 01:46:30 +0700

    `enableDefaultConfig` field is deprecated

--- home/truong/ssh.nix
+++ home/truong/ssh.nix
@@ -1,15 +1,18 @@
 { config, pkgs, inputs, ... }:
 {
-    programs.ssh.matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "yes";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = true;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+    programs.ssh = {
+        enableDefaultConfig = false;
+        matchBlocks."*" = {
+            forwardAgent = false;
+            addKeysToAgent = "yes";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = true;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+        };
     };
 }
