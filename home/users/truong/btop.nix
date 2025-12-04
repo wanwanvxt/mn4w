23fa207cd02ffd3cbbfe240b26848a9d7f349c74
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-04 21:25:37 +0700

    change graph symbol in `btop` settings

--- home/users/truong/btop.nix
+++ home/users/truong/btop.nix
@@ -10,7 +10,7 @@
             presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
             vim_keys = false;
             rounded_corners = true;
-            graph_symbol = "block";
+            graph_symbol = "braille";
             graph_symbol_cpu = "default";
             graph_symbol_mem = "default";
             graph_symbol_net = "default";
