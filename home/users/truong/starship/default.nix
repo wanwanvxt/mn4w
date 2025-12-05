4c351df5c4a2f4217e3b58da6a766af3e3889cfe
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-05 18:58:17 +0700

    update `starship` settings

--- home/users/truong/starship/default.nix
+++ home/users/truong/starship/default.nix
@@ -1,7 +1,30 @@
 { config, pkgs, inputs, ... }:
+let
+    bracketedSegmentsCfg = builtins.fromTOML (builtins.readFile ./bracketed-segments.toml);
+in
 {
     programs.starship = {
         enable = true;
-        settings = builtins.fromTOML (builtins.readFile ./bracketed-segments.toml);
+        settings = {
+            add_newline = false;
+            username = bracketedSegmentsCfg.username // {
+                show_always = true;
+            };
+            hostname = bracketedSegmentsCfg.hostname // {
+                ssh_only = false;
+            };
+            directory = bracketedSegmentsCfg.directory // {
+                truncate_to_repo = false;
+            };
+            time = bracketedSegmentsCfg.time // {
+                disabled = false;
+            };
+            status = bracketedSegmentsCfg.status // {
+                disabled = false;
+            };
+            shell = bracketedSegmentsCfg.shell // {
+                disabled = false;
+            };
+        } // bracketedSegmentsCfg;
     };
 }
