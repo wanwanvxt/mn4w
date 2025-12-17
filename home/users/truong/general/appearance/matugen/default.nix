e8deca08607ffdc7a4db14960763848d3978b8e3
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-17 08:34:08 +0700

    update `qtct` colors template

--- home/users/truong/general/appearance/matugen/default.nix
+++ home/users/truong/general/appearance/matugen/default.nix
@@ -18,28 +18,6 @@ in {
             version_check = false;
             fallback_color = "#9ece6a";
             caching = true;
-            custom_colors = {
-                link = {
-                    color = "#2980b9";
-                    blend = false;
-                };
-                visited = {
-                    color = "#9b59b6";
-                    blend = false;
-                };
-                negative = {
-                    color = "#da4453";
-                    blend = false;
-                };
-                neutral = {
-                    color = "#f67400";
-                    blend = false;
-                };
-                positive = {
-                    color = "#27ae60";
-                    blend = false;
-                };
-            };
         };
         templates =
             {}
@@ -50,7 +28,6 @@ in {
                 };
             })
             // (lib.optionalAttrs (config.qt.enable && config.qt.platformTheme.name == "qtct") {
-                kcolorscheme.input_path = "${config.xdg.configHome}/matugen/templates/kcolorscheme.colors";
                 qtct = {
                     input_path = "${config.xdg.configHome}/matugen/templates/qtct.conf";
                     output_path = "${config.xdg.configHome}/qt6ct/colors/qtct.conf";
