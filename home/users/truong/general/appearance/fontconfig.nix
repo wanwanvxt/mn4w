47c0ca09c961a69fc0fb38c8a2c3c12484272ea5
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-22 04:34:40 +0700

    add more noto font families

--- home/users/truong/general/appearance/fontconfig.nix
+++ home/users/truong/general/appearance/fontconfig.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
     home.packages = with pkgs; [
         noto-fonts
+        noto-fonts-lgc-plus
         noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
         noto-fonts-color-emoji
         liberation_ttf
         nerd-fonts._0xproto
