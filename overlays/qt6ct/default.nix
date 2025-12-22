834650ca85544a22a0332b6de1a269a391efacfe
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-22 11:52:34 +0700

    specific `qt6ct` version to match patch

--- overlays/qt6ct/default.nix
+++ overlays/qt6ct/default.nix
@@ -1,10 +1,18 @@
 final: prev: {
     kdePackages = prev.kdePackages.overrideScope (kdeFinal: kdePrev: {
-        qt6ct = kdePrev.qt6ct.overrideAttrs (oldAttrs: {
+        qt6ct = kdePrev.qt6ct.overrideAttrs (oldAttrs: rec {
+            version = "0.11";
+            src = prev.fetchFromGitLab {
+                domain = "www.opencode.net";
+                owner = "trialuser";
+                repo = "qt6ct";
+                tag = version;
+                hash = "sha256-aQmqLpM0vogMsYaDS9OeKVI3N53uY4NBC4FF10hK8Uw=";
+            };
+
             buildInputs =
                 (oldAttrs.buildInputs or [])
                 ++ (with kdeFinal; [
-                    qtdeclarative
                     kconfig
                     kcolorscheme
                     kiconthemes
