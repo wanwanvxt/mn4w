ba88be01e97168f6c8a6af694d31a6e3ebf56e85
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-12-29 00:12:58 +0700

    enable `tumbler` service

--- modules/shared/misc.nix
+++ modules/shared/misc.nix
@@ -1,8 +1,9 @@
 {...}: {
     services = {
+        gnome.gnome-keyring.enable = true;
         udisks2.enable = true;
         gvfs.enable = true;
-        gnome.gnome-keyring.enable = true;
+        tumbler.enable = true;
     };
 
     environment.pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
