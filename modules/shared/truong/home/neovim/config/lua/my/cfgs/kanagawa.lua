c98c9a977313f474ea4f14739372eb18be361a79
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-01 01:27:48 +0700

    neovim: change how to update colorscheme

--- modules/shared/truong/home/neovim/config/lua/my/cfgs/kanagawa.lua
+++ modules/shared/truong/home/neovim/config/lua/my/cfgs/kanagawa.lua
@@ -3,118 +3,11 @@ if not vim.g.is_tty then
     vim.pack.add({ utils.gh("rebelot/kanagawa.nvim") })
 
     -- config
-    local set_colorscheme = function()
-        local chameleon = vim.fn.stdpath("config") .. "/lua/my/chameleon.lua"
-        local colors = dofile(chameleon)
-
-        require("kanagawa").setup({
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            fg         = colors.text,
-                            fg_dim     = colors.subtext,
-                            fg_reverse = colors.base,
-
-                            bg_dim     = colors.surface,
-                            bg_gutter  = colors.surface,
-
-                            bg_m3      = colors.surface,
-                            bg_m2      = colors.surface,
-                            bg_m1      = colors.surface,
-                            bg         = colors.base,
-                            bg_p1      = colors.highlight,
-                            bg_p2      = colors.highlight,
-
-                            special    = colors.subtext,
-                            nontext    = colors.overlay,
-                            whitespace = colors.overlay,
-
-                            bg_search  = colors.bg_accent_1,
-                            bg_visual  = colors.bg_accent_1,
-
-                            pmenu      = {
-                                fg       = colors.text,
-                                fg_sel   = "none",
-                                bg       = colors.highlight,
-                                bg_sel   = colors.bg_accent_1,
-                                bg_sbar  = colors.surface,
-                                bg_thumb = colors.overlay,
-                            },
-                            float      = {
-                                fg        = colors.text,
-                                bg        = colors.highlight,
-                                fg_border = colors.overlay,
-                                bg_border = colors.highlight,
-                            },
-                        },
-                        syn = {
-                            string     = colors.fg_green,
-                            variable   = "none",
-                            number     = colors.fg_red,
-                            constant   = colors.fg_yellow,
-
-                            identifier = colors.fg_cyan,
-                            parameter  = colors.fg_magenta,
-                            fun        = colors.fg_blue,
-
-                            statement  = colors.fg_magenta,
-                            keyword    = colors.fg_magenta,
-                            operator   = colors.subtext,
-
-                            preproc    = colors.fg_red,
-                            type       = colors.fg_cyan,
-                            regex      = colors.rg_red,
-
-                            deprecated = colors.overlay,
-                            comment    = colors.muted,
-                            punct      = colors.muted,
-
-                            special1   = colors.fg_accent_1,
-                            special2   = colors.fg_accent_2,
-                            special3   = colors.fg_accent_3,
-                        },
-                        vcs = {
-                            added   = colors.fg_green,
-                            removed = colors.fg_red,
-                            changed = colors.fg_yellow,
-                        },
-                        diff = {
-                            add    = colors.bg_green,
-                            delete = colors.bg_red,
-                            change = colors.bg_blue,
-                            text   = colors.bg_yellow,
-                        },
-                        diag = {
-                            ok      = colors.fg_green,
-                            error   = colors.fg_red,
-                            warning = colors.fg_yellow,
-                            info    = colors.fg_blue,
-                            hint    = colors.fg_hint,
-                        },
-                        term = {
-                            colors.black,
-                            colors.rg_red,
-                            colors.rg_green,
-                            colors.rg_yellow,
-                            colors.rg_blue,
-                            colors.rg_magenta,
-                            colors.rg_cyan,
-                            colors.white,
-                            colors.bright_black,
-                            colors.fg_red,
-                            colors.fg_green,
-                            colors.fg_yellow,
-                            colors.fg_blue,
-                            colors.fg_magenta,
-                            colors.fg_cyan,
-                            colors.bright_white,
-                        },
-                    },
-                },
-            },
-        })
-        vim.cmd.colorscheme("kanagawa")
+    local update_colorscheme = function()
+        local ok = pcall(vim.cmd.colorscheme, "chameleon")
+        if not ok then
+            vim.cmd.colorscheme("kanagawa")
+        end
     end
 
     local signal_handler = function()
@@ -145,6 +38,6 @@ if not vim.g.is_tty then
         })
     end
 
-    set_colorscheme()
+    update_colorscheme()
     signal_handler()
 end
