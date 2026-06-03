76cd297e3db2957bdcfd0826615662c2de287046
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-03 10:06:25 +0700

    use zapret to bypass some websites

--- modules/shared/truong/networking.nix
+++ modules/shared/truong/networking.nix
@@ -6,4 +6,12 @@
 
     location.provider = "geoclue2";
     services.geoclue2.enable = true;
+
+    services.zapret = {
+        enable = true;
+        params = [
+            "--dpi-desync=multisplit"
+            "--dpi-desync-split-pos=2"
+        ];
+    };
 }
