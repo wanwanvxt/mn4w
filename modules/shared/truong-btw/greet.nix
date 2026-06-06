cc87fc82fe113e2f5038879a7bebd7fe9f11432c
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-06 17:32:19 +0700

    use path to tuigreet bin instead of add to system

--- modules/shared/truong-btw/greet.nix
+++ modules/shared/truong-btw/greet.nix
@@ -1,13 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
     programs.uwsm.enable = true;
 
-    environment.systemPackages = [ pkgs.tuigreet ];
     services.greetd = {
         enable = true;
         settings = {
             default_session = {
                 command = ''
-                    tuigreet --time --remember --remember-session --asterisks \
+                    ${lib.getExe pkgs.tuigreet} --time --remember --remember-session --asterisks \
                     --cmd 'uwsm start default'
                 '';
                 user = "greeter";
