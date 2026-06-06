ebddff7cceb42334b79970d01a5bbffdfbf50086
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-06 10:52:33 +0700

    add a font to console tty

--- modules/shared/truong-btw/locale.nix
+++ modules/shared/truong-btw/locale.nix
@@ -1,5 +1,9 @@
-{
+{ pkgs, ... }: {
     time.timeZone = "Asia/Ho_Chi_Minh";
     i18n.defaultLocale = "en_US.UTF-8";
-    console.keyMap = "us";
+    console = {
+        keyMap = "us";
+        font = "ter-132n";
+        packages = [ pkgs.terminus_font ];
+    };
 }
