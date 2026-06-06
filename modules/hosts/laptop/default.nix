289446d26b18f3e31c85d230c3927078d80b7e29
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-06 14:01:47 +0700

    host/laptop: add amd ucode

--- modules/hosts/laptop/default.nix
+++ modules/hosts/laptop/default.nix
@@ -2,6 +2,13 @@
     imports = [
         ../../shared/truong-btw
         ./hardware-config.nix
-        ./graphics.nix
     ];
+
+    hardware.cpu.amd.updateMicrocode = true;
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.prime = {
+        amdgpuBusId = "PCI:5@0:0:0";
+        nvidiaBusId = "PCI:1@0:0:0";
+    };
 }
--- modules/hosts/laptop/graphics.nix
+++
@@ -1,7 +0,0 @@
-{
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.prime = {
-        amdgpuBusId = "PCI:5@0:0:0";
-        nvidiaBusId = "PCI:1@0:0:0";
-    };
-}
