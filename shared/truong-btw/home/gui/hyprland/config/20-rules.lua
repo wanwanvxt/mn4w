ad9043bf7f4e5cda4257c9b186ffb36521ec2258
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-02 21:02:18 +0700

    hyprland: fix thunar window randomly class

--- shared/truong-btw/home/gui/hyprland/config/20-rules.lua
+++ shared/truong-btw/home/gui/hyprland/config/20-rules.lua
@@ -3,7 +3,7 @@ hl.window_rule({ match = { class = ".*" }, immediate = true })
 hl.window_rule({ match = { xwayland = true }, no_blur = true })
 
 hl.window_rule({ float = true, match = { class = "^$" , title = "^Select what to share$" }})
-hl.window_rule({ float = true, match = { class = "^thunar$", title = "^Rename .*" } })
+hl.window_rule({ float = true, match = { class = "^(?i)thunar$", title = "^Rename .*" } })
 hl.window_rule({ float = true, match = { class = "^xarchiver$", title = "^Extract files$" } })
 hl.window_rule({ float = true, match = { class = "^steam$", title = "^(Steam Settings|Add Non-Steam Game)$" } })
 hl.window_rule({ float = true, size = {1000, 800}, match = { class = "^xdg-desktop-portal-gtk$" } })
