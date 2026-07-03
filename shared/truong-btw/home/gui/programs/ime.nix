580de286d6cbeae2876da3bc836c2a12166dafa0
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-03 21:17:08 +0700

    ime/fcitx5: disable toggle lanuage when press shift_l

--- shared/truong-btw/home/gui/programs/ime.nix
+++ shared/truong-btw/home/gui/programs/ime.nix
@@ -1,40 +1,39 @@
 { pkgs, config, lib, ... }: {
-    config = lib.mkIf config.truong-btw.enable (lib.mkMerge [
-        {
-            i18n.inputMethod = {
-                enable = true;
-                type = "fcitx5";
-                fcitx5 = {
-                    waylandFrontend = true;
-                    addons = [ pkgs.fcitx5-bamboo ];
-                    settings = {
-                        inputMethod = {
-                            GroupOrder."0" = "Default";
-                            "Groups/0" = {
-                                Name = "Default";
-                                "Default Layout" = "us";
-                                DefaultIM = "bamboo";
-                            };
-                            "Groups/0/Items/0".Name = "keyboard-us";
-                            "Groups/0/Items/1".Name = "bamboo";
+    config = lib.mkIf config.truong-btw.enable {
+        i18n.inputMethod = {
+            enable = true;
+            type = "fcitx5";
+            fcitx5 = {
+                waylandFrontend = true;
+                addons = [ pkgs.fcitx5-bamboo ];
+                settings = {
+                    inputMethod = {
+                        GroupOrder."0" = "Default";
+                        "Groups/0" = {
+                            Name = "Default";
+                            "Default Layout" = "us";
+                            DefaultIM = "bamboo";
                         };
-                        globalOptions = {
-                            "Hotkey/TriggerKeys"."0" = "Super+space";
-                        };
-                        addons = {
-                            bamboo.globalSection = {
-                                InputMethod = "Telex";
-                                OutputCharset = "Unicode";
-                                SpellCheck = "True";
-                                AutoNonVnRestore = "True";
-                                ModernStyle = "True";
-                                FreeMarking = "True";
-                                DisplayUnderline = "True";
-                            };
+                        "Groups/0/Items/0".Name = "keyboard-us";
+                        "Groups/0/Items/1".Name = "bamboo";
+                    };
+                    globalOptions = {
+                        "Hotkey/TriggerKeys"."0" = "Super+space";
+                        "Hotkey/AltTriggerKeys"."0" = "";
+                    };
+                    addons = {
+                        bamboo.globalSection = {
+                            InputMethod = "Telex";
+                            OutputCharset = "Unicode";
+                            SpellCheck = "True";
+                            AutoNonVnRestore = "True";
+                            ModernStyle = "True";
+                            FreeMarking = "True";
+                            DisplayUnderline = "True";
                         };
                     };
                 };
             };
-        }
-    ]);
+        };
+    };
 }
