b9634eea2a29cf01a81fe2bf1820f5b62cca0ba3
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-03 12:33:31 +0700

    thunar: fix archive plugin not recognize xarchiver

--- shared/truong-btw/home/gui/programs/thunar.nix
+++ shared/truong-btw/home/gui/programs/thunar.nix
@@ -1,17 +1,29 @@
 { pkgs, config, lib, ... }:
 let
     helpers = import ../../helpers.nix lib;
+
+    pkgThunar = pkgs.thunar.override {
+        thunarPlugins = with pkgs; [
+            thunar-volman
+            (thunar-archive-plugin.overrideAttrs (oldAttrs: {
+                postInstall = (oldAttrs.postInstall or "") + ''
+                    cp ${pkgXarchiver}/libexec/thunar-archive-plugin/* $out/libexec/thunar-archive-plugin/
+                '';
+            }))
+        ];
+    };
+    pkgXarchiver = pkgs.xarchiver.overrideAttrs (oldAttrs: {
+        postFixup = (oldAttrs.postFixup or "") + ''
+            wrapProgram $out/bin/xarchiver \
+                --prefix PATH : ${lib.makeBinPath [ pkgs.rar ]}
+        '';
+    });
 in
 {
     config = lib.mkIf config.truong-btw.enable {
-        home.packages = with pkgs; [
-            (thunar.override {
-                thunarPlugins = with pkgs; [
-                    thunar-volman
-                    thunar-archive-plugin
-                ];
-            })
-            xarchiver
+        home.packages = [
+            pkgThunar
+            pkgXarchiver
         ];
 
         home.sessionVariables.FILE_MANAGER = "thunar";
