e0fc4f2bb3e39f8d2a4c7ea2587cb059c0ad413b
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-07 03:12:50 +0700

    hyprshot: check userDirs config is enabled

--- shared/truong-btw/home/gui/hyprland/hyprshot.nix
+++ shared/truong-btw/home/gui/hyprland/hyprshot.nix
@@ -1,13 +1,17 @@
 { config, lib, ... }:
 let
     hyprlandCfg = config.wayland.windowManager.hyprland;
-    xdgCfg = config.xdg;
+    xdgUserDirsCfg = config.xdg.userDirs;
 in
 {
     config = lib.mkIf config.truong-btw.enable {
         programs.hyprshot = {
             enable = hyprlandCfg.enable;
-            saveLocation = "${xdgCfg.userDirs.pictures or "~"}/screenshots";
+            saveLocation = "${
+                if xdgUserDirsCfg.enable
+                then xdgUserDirsCfg.pictures
+                else "~"
+            }/screenshots";
         };
     };
 }
