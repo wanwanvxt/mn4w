8a66a3ac6172beb2940e949f66ba496db3fe8cc8
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-06 23:26:33 +0700

    hyprland: add nemo floating rules

--- shared/truong-btw/home/gui/hyprland/config/20-rules.lua
+++ shared/truong-btw/home/gui/hyprland/config/20-rules.lua
@@ -3,12 +3,13 @@ hl.window_rule({ match = { class = ".*" }, immediate = true })
 hl.window_rule({ match = { xwayland = true }, no_blur = true })
 
 hl.window_rule({ float = true, match = { class = "^$" , title = "^Select what to share$" }})
-hl.window_rule({ float = true, match = { class = "^(?i)thunar$", title = "negative:.* - Thunar$" } })
+hl.window_rule({ float = true, match = { class = "^nemo$", title = "^.* Properties$" } })
 hl.window_rule({ float = true, match = { class = "^xarchiver$", title = "^Extract files$" } })
 hl.window_rule({ float = true, match = { class = "^steam$", title = "^(Steam Settings|Add Non-Steam Game)$" } })
 hl.window_rule({ float = true, size = {1000, 800}, match = { class = "^xdg-desktop-portal-gtk$" } })
 hl.window_rule({ float = true, size = {1000, 800}, match = { class = "^firefox$", title = "^Library$" } })
 hl.window_rule({ float = true, size = {800, 400},  match = { class = "^firefox$", title = "^About Mozilla Firefox$" } })
+hl.window_rule({ float = true, size = {800, 400},  match = { class = "^nemo$", title = "^Edit Bookmarks$" } })
 hl.window_rule({ float = true, size = {400, 800},  match = { class = "^steam$", title = "^Friends List$" } })
 hl.window_rule({
     float = true, pin = true, size = {640, 360}, move = {"(monitor_w-700)", "(monitor_h-400)"},
