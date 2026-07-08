f4948fc0a689200c65204950d48026a9f3ecde09
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-09 01:28:33 +0700

    enable upower, power-profiles services

--- shared/truong-btw/programs.nix
+++ shared/truong-btw/programs.nix
@@ -40,6 +40,8 @@
         };
 
         services = {
+            upower.enable = true;
+            power-profiles-daemon.enable = true;
             gvfs.enable = true;
             udisks2.enable = true;
             gnome.gnome-keyring.enable = true;
