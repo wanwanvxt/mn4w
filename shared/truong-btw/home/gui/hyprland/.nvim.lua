52d15b830b78e60c643ff0c1eb33d8b028f3a518
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-07-08 18:07:09 +0700

    use .nvim.lua file to config luals

--- shared/truong-btw/home/gui/hyprland/.nvim.lua
+++ shared/truong-btw/home/gui/hyprland/.nvim.lua
@@ -1,1 +1,17 @@
+vim.lsp.config.lua_ls = {
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    "/etc/profiles/per-user/${env:USER}/share/hypr/stubs"
+                },
+            },
+            diagnostics = { globals = { "hl" } },
+            telemetry = { enable = false },
+        },
+    },
+}
+
 vim.lsp.enable({ "lua_ls", "bashls" })
--- shared/truong-btw/home/programs/neovim/.nvim.lua
+++ shared/truong-btw/home/programs/neovim/.nvim.lua
@@ -1,1 +1,18 @@
+vim.lsp.config.lua_ls = {
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    "$VIMRUNTIME/lua",
+                    "$XDG_DATA_HOME/nvim/site/pack/core/opt",
+                },
+            },
+            diagnostics = { globals = { "vim" } },
+            telemetry = { enable = false },
+        },
+    },
+}
+
 vim.lsp.enable({ "lua_ls" })
